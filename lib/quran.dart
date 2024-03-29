57c8f646746a103e2cef65ae30e5aac235340608
abiin21 <keyla.putrie.ti.21@cic.id>
2024-03-29 21:37:10 +0700

    second commit

--- lib/quran.dart
+++ lib/quran.dart
@@ -25,6 +25,94 @@ class Quran extends StatelessWidget {
                           begin: Alignment(0.71, -0.71),
                           end: Alignment(-0.71, 0.71),
                           colors: [Color(0xFFDF98FA), Color(0xFF9055FF)])),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Al-Fatiah',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 26,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w400,
+                          height: 0,
+                        ),
+                      ),
+                      Text(
+                        'The Opening',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w400,
+                          height: 0,
+                        ),
+                      ),
+                      Container(
+                        width: 200,
+                        decoration: ShapeDecoration(
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                              width: 1,
+                              strokeAlign: BorderSide.strokeAlignCenter,
+                              color:
+                                  Colors.white.withOpacity(0.3499999940395355),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 134,
+                        height: 21,
+                        clipBehavior: Clip.antiAlias,
+                        decoration: BoxDecoration(),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              'MECCAN',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 14,
+                                fontFamily: 'Poppins',
+                                fontWeight: FontWeight.w400,
+                                height: 0,
+                              ),
+                            ),
+                            const SizedBox(width: 5),
+                            Container(
+                              width: 4,
+                              height: 4,
+                              decoration: ShapeDecoration(
+                                color: Colors.white
+                                    .withOpacity(0.3499999940395355),
+                                shape: OvalBorder(),
+                              ),
+                            ),
+                            const SizedBox(width: 5),
+                            Text(
+                              '7 VERSES',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 14,
+                                fontFamily: 'Poppins',
+                                fontWeight: FontWeight.w400,
+                                height: 0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Image.asset(
+                        "assets/bismillah.png",
+                        width: 250,
+                        height: 150,
+                      )
+                    ],
+                  ),
                 ),
                 Image.asset(
                   "assets/Quran.png",
